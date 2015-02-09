b88544e3da29a1284d0de0a37b85c9030b629fa2
isabel3 <isabel_lozano20@hotmail.com>
2015-02-09 15:30:39 -0500

    Inicial commit

---
+++ db/src/java/isabel2/LoginServlet.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isabel2;
+
+import java.io.IOException;  
+import java.io.PrintWriter;  
+  
+import javax.servlet.RequestDispatcher;  
+import javax.servlet.ServletException;  
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+import javax.servlet.http.HttpSession;  
+  
+import isabel.LoginDao;  
+  
+public class LoginServlet extends HttpServlet{  
+  
+    private static final long serialVersionUID = 1L;  
+  
+    public void doPost(HttpServletRequest request, HttpServletResponse response)    
+            throws ServletException, IOException {    
+  
+        response.setContentType("text/html");    
+        PrintWriter out = response.getWriter();    
+          
+        String n=request.getParameter("username");    
+        String p=request.getParameter("userpass");   
+          
+        HttpSession session = request.getSession(false);  
+        if(session!=null)  
+        session.setAttribute("name", n);  
+  
+        if(LoginDao.validate(n, p)){    
+            RequestDispatcher rd=request.getRequestDispatcher("welcome.jsp");    
+            rd.forward(request,response);    
+        }    
+        else{    
+            out.print("<p style=\"color:red\">Su Usuario o Contraseña son Incorrectos</p>");    
+            RequestDispatcher rd=request.getRequestDispatcher("index.jsp");    
+            rd.include(request,response);    
+        }    
+  
+        out.close();    
+    }    
+}   
