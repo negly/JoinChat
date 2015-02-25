191ebd22b790d2f50094cf5777b41a3aa1b50a33
caladfelipe <felipe.calad@alegra.co>
2015-02-25 15:39:20 -0500

    Quitar aplicación de webRTC obsoleta

--- webRTC/video/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Video(models.Model):
-	ip = models.CharField(max_length=200)
-	user = models.CharField(max_length=100)
-	date_connection = models.DateTimeField('Date of connection')
-	# def __str__(self):
- #        return self.ip
