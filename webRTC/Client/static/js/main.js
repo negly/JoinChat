15d2efbe354043e11668a402fda5b43e2d4aca95
Juan <juanchoperez70@gmail.com>
2015-03-02 20:27:05 -0500

    Add license to webRTC

--- webRTC/Client/static/js/main.js
+++ webRTC/Client/static/js/main.js
@@ -1,3 +1,31 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2015 
+
+// John Congote <jcongote@gmail.com>
+// Felipe Calad
+// Isabel Lozano
+// Juan Diego Perez
+// Joinner Ovalle
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 var token = prompt("Please enter the id of the conversation", "123456789");
 var ws = new WebSocket("ws://negly14.koding.io:7000/ws/"+token);
 var configuration = {iceServers: [{ url: 'stun:negly14.koding.io:3478' }]};
