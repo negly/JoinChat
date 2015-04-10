4ce69ef9a96ac50c46cba2f5b8e27a548ac96244
caladfelipe <felipe.calad@alegra.co>
2015-04-09 22:56:42 -0500

    Enviar mensajes por el chat con presionar enter, no solo con el botón de enviar. También poder enviar por el datachannel comandos

--- phpApp/app/webroot/js/main.js
+++ phpApp/app/webroot/js/main.js
@@ -30,7 +30,7 @@
 
 // var chatId = false;
 var chatId = '111112';
-var localStream, remoteStream, pc, ws;
+var localStream, remoteStream, pc, ws, channel;
 $(document).ready(function() {
     ws = new WebSocket('ws://kladfelipe.koding.io:7000/ws/' + chatId);
     var configuration = {iceServers: [
@@ -43,10 +43,10 @@ $(document).ready(function() {
     ]};
     var initiator;
     pc = new webkitRTCPeerConnection(configuration, {optional: [{RtpDataChannels: true}]});
-    var channel;
 
     $(window).bind('beforeunload', function(){
         if (pc && pc.close) {
+            channel.close();
             pc.close();
         }
     });
@@ -69,33 +69,44 @@ $(document).ready(function() {
     ws.onmessage = initiatorCtrl;
 
     function init() {
+        var channelOnMessage = function (evt) {
+            var objReceived = JSON.parse(evt.data);
+            if (objReceived.command) {
+                var commandParts = objReceived.command.split('.');
+                var args = [];
+                if (objReceived.args) {
+                    args = objReceived.args;
+                }
+                window[commandParts[0]][commandParts[1]](args);
+            }
+            if (objReceived.msg) {
+                var msg = objReceived.msg;
+                console.info(evt);
+                createMsg(false, msg);
+            }  
+        };
+        var channelOnOpen = function () {
+            console.log("Channel " + channel.label + " is open");
+            enableChatFields();
+        };
+        var channelOnClose = function (evt) {
+            console.log('RTCDataChannel closed.');
+        };
+
         if (initiator) {
             var channelOptions = { reliable: false };
-            channel = pc.createDataChannel("chat"+chatId, channelOptions);
-            channel.onmessage = function (evt) {
-                console.info(evt);
-                createMsg(false, evt.data);
-            };
+            channel = pc.createDataChannel("chat" + chatId, channelOptions);
 
-            channel.onopen = function (evt) {
-                console.log("Channel " + channel.label + " is open");
-                enableChatFields();
-            };
-            
-            channel.onclose = function (evt) {
-                console.log('RTCDataChannel closed.');
-            };
+            channel.onmessage = channelOnMessage;
+            channel.onopen = channelOnOpen;
+            channel.onclose = channelOnClose;
         } else {
             pc.ondatachannel = function(evt){
                 channel = evt.channel;
-                channel.onopen = function () {
-                    console.log("Channel " + channel.label + " is open");
-                    enableChatFields();
-                };
-                channel.onmessage = function (evt) {
-                    console.info(evt);
-                    createMsg(false, evt.data);
-                };
+
+                channel.onopen = channelOnOpen;
+                channel.onmessage = channelOnMessage;
+                channel.onclose = channelOnClose;
             };
         }
         connect();
@@ -213,13 +224,32 @@ $(document).ready(function() {
     }
 
     function sendChatMessage() {
-        channel.send($("#msg").val());
+        var objToSend = {
+            msg: $("#msg").val()
+        };
+        channel.send(JSON.stringify(objToSend));
         createMsg(true, $("#msg").val());
         $("#msg").val("");
     }
 
     $("#sendChatBtn").on('click', function() {
         sendChatMessage();
+    });
+
+    var keydown, lastKeydown;
+    $("#msg").on('keydown', function(event) {
+        if (keydown && (lastKeydown !== keydown)) {
+            lastKeydown = keydown;
+        } 
+        keydown = event.which;
+    });
+    $("#msg").on('keypress', function(event) {
+        // 16 - Shift
+        // 13 - Enter
+        if (lastKeydown !== 16 && keydown === event.which && keydown == 13) {
+            sendChatMessage();
+            event.preventDefault();
+        }
     });
 
     jQuery.fn.attachStream = function(stream) {
@@ -279,7 +309,7 @@ function createMsg(localUser, msg) {
         containerClass = 'bubble-right';
     }
 
-    $msgContainer = $("<div>").addClass('bubble').addClass(containerClass).html("<div class='pointer'></div>" + msg);
+    $msgContainer = $("<div>").addClass('bubble').addClass(containerClass).html("<div class='pointer'></div>" + msg.replace(/(\r[\n]?)|(\n[\r]?)/, "<br>"));
     
     $("#textchat").append($msgContainer);
 }
